17bbea2fd8dba4faff6bd7d9819bf6ec6e8ae506
Gabriel Rodrigues Bueno <105130300+GabrielRodriguesB@users.noreply.github.com>
2023-07-06 22:17:02 -0300

    Arquivos da linkpage

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let interruptor = document.getElementById('interruptor');
+let root= document.documentElement;
+
+
+interruptor.addEventListener('click', ()=>{
+
+	root.classList.toggle('tema-claro');
+
+})
+
+
+const verMais= document.getElementById('ver__mais');
+const projetos= document.querySelector('.projetos');
+
+
+
+verMais.addEventListener('click', ()=>{
+	projetos.classList.toggle('hidden');
+
+})
